56759689d906e407805ccb4eea82b4b51cef3426
E-Lop <edmondolopez@gmail.com>
2022-07-15 16:18:36 +0200

    estetica header

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div id="app">
+    <MyHeader />
     <MainComponent />
   </div>
 </template>
 
 <script>
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import MainComponent from './components/MainComponent.vue';
+import MyHeader from './components/MyHeader.vue';
 
 export default {
   name: 'App',
   components: {
     MainComponent,
+    MyHeader,
   },
 };
 </script>
 
 <style lang="scss">
 @import './assets/style/common.scss';
+@import './assets/style/variables.scss';
 </style>
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,4 +1,42 @@
-<template></template>
+<template>
+  <div class="entire_header">
+    <div class="container">
+      <div class="navbar">
+        <div class="navbar_left">
+          <a class="navbar-brand" href="#">
+            <img
+              src="../assets/img/avadabarbers-logo-x1.png"
+              alt="Logo Avada Barbers"
+            />
+          </a>
+        </div>
+        <div class="navbar_icons">
+          <!-- icona shopping cart -->
+          <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+          <!-- Collapse button -->
+          <button class="navbar-toggler" type="button">
+            <span class="burer"><i class="fa-solid fa-bars"></i></span>
+          </button>
+        </div>
+      </div>
+      <!-- contenuto al centro dell'header -->
+      <div class="header_hero_content">
+        <div class="header_hero_left">
+          <h1>Barber Shop</h1>
+          <div class="golden_bar"></div>
+          <div class="claim">The Pinnacle of Male Grooming</div>
+          <button class="learn_btn">LEARN MORE</button>
+        </div>
+        <div class="header_hero_right">
+          <img
+            src="../assets/img/avadabarbers_hero_focalmirror-600x825.png"
+            alt="immagine barbiere al lavoro"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
@@ -6,4 +44,62 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '../assets/style/variables.scss';
+.entire_header {
+  background-image: url('../assets/img/avadabarbers-homepage-hero-bg.jpg');
+  object-fit: cover;
+  background-position: top;
+  background-repeat: no-repeat;
+}
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 20px;
+  i {
+    color: white;
+    font-size: 1.5rem;
+  }
+}
+.navbar-brand {
+  img {
+    width: 170px;
+  }
+}
+.header_hero_content {
+  width: 100%;
+  display: flex;
+}
+.header_hero_left {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  color: white;
+  h1 {
+    font-size: 3rem;
+  }
+  .golden_bar {
+    height: 2px;
+    width: 200px;
+    background-color: $main_color;
+    margin-block: 30px;
+  }
+  .claim {
+    margin-bottom: 30px;
+    font-size: 1.2rem;
+    color: $secondary_color;
+  }
+  .learn_btn {
+    color: $main_color;
+    border: 1px solid $main_color;
+    padding: 15px 30px;
+    background-color: transparent;
+  }
+}
+.header_hero_right {
+  width: 50%;
+}
+</style>
